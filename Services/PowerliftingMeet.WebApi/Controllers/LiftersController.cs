dea449987d5cfa675c265949fce4e33f89adfd9b
Drew Young <drewyoung@quickenloans.com>
2017-07-29 14:16:30 -0400

    Check in for basic web api component with lifter controller

---
+++ Services/PowerliftingMeet.WebApi/Controllers/LiftersController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace PowerliftingMeet.WebApi.Controllers
+{
+    [RoutePrefix("api/lifter")]
+    public class LiftersController : ApiController
+    {
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok("Lifter");
+        }
+    }
+}
